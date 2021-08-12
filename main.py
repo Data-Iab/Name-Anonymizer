be25b2fbfa147d616c602187312ef5560516f0ba
Taha Azzim <62215012+Data-Iab@users.noreply.github.com>
2021-08-12 15:46:15 +0100

    add executing time

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import numpy as np
+
 from name_identifier.name_identifier import NameIdentifier
 from file_handler.file_loader import Loader
 from file_handler.file_saver import Saver
 from anonymizers.anonymizer import Anonymizer
 import argparse
+import time
 
 
 def main():
@@ -20,6 +23,7 @@ def main():
     input_dir = args.input_dir
     output_dir = args.output_dir
 
+    start = time.time()
     name_identifier = NameIdentifier()
     loader = Loader(input_dir)
     saver = Saver(output_dir)
@@ -28,7 +32,10 @@ def main():
     text_list = loader.load_files()
     text_list = name_identifier.find_entities(text_list)
     text_list = anonymizer.anonymize(text_list)
+
     saver.save(text_list)
+    end = time.time()
+    print(f'Executed in {end-start}s')
 
 
 if __name__ == '__main__':
