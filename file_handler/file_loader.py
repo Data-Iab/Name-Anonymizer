773dbda987e923d7aef014bf487d9c5759108633
Taha Azzim <62215012+Data-Iab@users.noreply.github.com>
2021-08-12 15:49:23 +0100

    add STloader class for reports loading in Streamlit

--- file_handler/file_loader.py
+++ file_handler/file_loader.py
@@ -1,18 +1,27 @@
 from name_identifier.text import Text
 import glob
+from typing import List
+import warnings
 
 
 class Loader:
-    def __init__(self, input_dir, encoding='utf-8'):
+    def __init__(self, input_dir=None, paths=None, encoding='utf-8'):
         """
 
         :param input_dir: directory path of readable files
+        :param paths: readable files paths. Pass either input_dir
+                      or list of paths, if both are passed priority goes to paths
         :param encoding: text encoding
         """
         self.encoding: str = encoding
         self.input_dir: str = input_dir
-        self.text_list = []
-        self.paths = sorted(glob.glob(input_dir + '/*'))
+        self.text_list: List[str] = []
+        self.paths = paths
+        if paths is None:
+            try:
+                self.paths: List[str] = sorted(glob.glob(input_dir + '/*'))
+            except:
+                print()
 
     def add_txt(self, txt):
         self.text_list.append(txt)
@@ -24,4 +33,18 @@ class Loader:
                 content = f.read()
                 txt = Text(path=path, content=content)
                 self.add_txt(txt)
+        return self.text_list
+
+
+class STLoader(Loader):
+    def __init__(self, uploaded, **kwargs):
+        super().__init__(uploaded, **kwargs)
+        self.uploaded = uploaded
+
+    def load_files(self):
+        """Loads files to self.text_list as Text objects"""
+        for file in self.uploaded:
+            content = file.read().decode(self.encoding)
+            txt = Text(path=file.name, content=content)
+            self.add_txt(txt)
         return self.text_list
